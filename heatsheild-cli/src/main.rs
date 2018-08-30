8f32aae3a606e281efe87aefd6e076ac0b4eeaf8
technetos <mrgould93@gmail.com>
2018-08-30 12:07:50 -0400

    add adduser and gensalt

--- heatsheild-cli/src/main.rs
+++ heatsheild-cli/src/main.rs
@@ -1,7 +1,50 @@
 extern crate heatshield;
 
-use heatshield::salt;
+#[macro_use]
+extern crate quicli;
+extern crate uuid;
 
-fn main() {
+use quicli::prelude::*;
+use uuid::Uuid;
 
+use heatshield::{
+    account::{controller::AccountController, model::Account},
+    controller::ResourceController,
+    salt::controller::SaltController,
+    validate::Validator,
+};
+
+#[derive(StructOpt, Debug)]
+struct AddUser {
+    #[structopt(short = "u")]
+    username: String,
+    #[structopt(short = "p")]
+    password: String,
+    #[structopt(short = "e")]
+    email: String,
 }
+
+#[derive(StructOpt, Debug)]
+#[structopt(raw(setting = "structopt::clap::AppSettings::InferSubcommands"))]
+enum Opt {
+    #[structopt(name = "gensalt", alias = "gs")]
+    GenSalt,
+    #[structopt(name = "adduser", raw(aliases = r#"&["add"]"#))]
+    AddUser(AddUser),
+}
+
+main!(|args: Opt| match args {
+  Opt::GenSalt => { SaltController.create_salt(); }
+  Opt::AddUser(AddUser { username, password, email }) => {
+    let account = Account {
+        username: Some(username),
+        password: Some(password),
+        email: Some(email),
+        uuid: Some(Uuid::new_v4()),
+    };
+
+    account.validate().expect("Invalid account");
+    AccountController.create(&account).expect("Unable to create account"); 
+    println!("Account created successfully!");
+  }
+});
